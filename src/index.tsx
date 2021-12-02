8085638eff0ee98256954212dcb5c6dd19e5b2db
Antonio Pantano <ntnpantano@gmail.com>
2021-12-02 10:07:38 +0100

    Add Sidebar button

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
+import { initializeIcons } from '@fluentui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { IBThemeProvider } from '../Providers/ThemeProvider';
 import App from './App';
 
 const root = document.getElementById('root');
+initializeIcons();
 
 ReactDOM.render(
   <StrictMode>
