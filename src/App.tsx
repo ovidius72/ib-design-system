2a8ba0b7784d474d61736298322f740694991e39
palloncino <antonio.guiotto.dev@gmail.com>
2021-12-27 12:09:21 +0100

    filters pt. 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable */
 import React from 'react';
 import { IBBox } from './components/Box/Box';
+import Filters from './components/Filters';
 
 export const App = () => {
-  
+  const mockedItems = [
+    { id: 0, name: 'input_text', label: 'Search All', type: 0 },
+    { id: 1, name: 'input_number', label: 'Filter by Number', type: 1 },
+    { id: 2, name: 'input_select', label: 'Selection', type: 2 },
+    { id: 3, name: 'input_currency', label: 'by Number (Currency)', type: 3 },
+  ];
 
   return (
     <IBBox p={2}>
@@ -11,8 +17,10 @@ export const App = () => {
         App.tsx: You can test your components here for a realistic experience
       </IBBox>
       <IBBox mb={4}>
-        Component: <strong>start testing a component here</strong>
+        Component: <strong>Filters</strong>
       </IBBox>
+
+      <Filters items={mockedItems} />
     </IBBox>
   );
 };
