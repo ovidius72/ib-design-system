c36b422e796c048972a1da05f5dc7b115cbcdebc
palloncino <antonio.guiotto.dev@gmail.com>
2021-12-27 16:35:21 +0100

    filters pt. 4

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,11 +1,82 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 import { IBFiltersProps, FilterItem } from './filters.types';
 import { SearchBox, ISearchBoxStyles } from '@fluentui/react/lib/SearchBox';
+import { TextField } from '@fluentui/react/lib/TextField';
 import { IBBox } from '../Box/Box';
 
 const Filters = ({ items = [] }: IBFiltersProps) => {
-  const searchBoxStyles: Partial<ISearchBoxStyles> = { root: { width: 200 } };
+  const searchBoxStyles = { root: { width: 200 } };
+  const textBoxStyles = { root: { width: 200 } };
+
+  // +----------------------------------
+  // | 0) text input (search)
+  // +----------------------------------
+
+  // +----------------------------------
+  // | 1) Numeric
+  // +----------------------------------
+  const [numberInputError, setNumberInputError] = useState('');
+  const [numberInputState, setNumberInputState] = useState(0);
+
+  const handleNumericInputChange: any = (value: string) => {
+    const onlyNumberRegex = new RegExp('^[0-9]*$');
+
+    const isOnlyNumeric = onlyNumberRegex.test(value);
+
+    if (!value) {
+      setNumberInputState(0);
+      setNumberInputError('');
+      return;
+    }
+
+    if (!isOnlyNumeric) {
+      setNumberInputState(0);
+      setNumberInputError('Invalid format');
+      return;
+    }
+
+    const num = Number(value);
+    setNumberInputState(num);
+    setNumberInputError('');
+  };
+
+  // +----------------------------------
+  // | 3) currency
+  // +----------------------------------
+  // const [currencyInputError, setCurrencyInputError] = useState('');
+  // const [currencyInputLabel, setCurrencyInputLabel] = useState('');
+  // const [currencyInputValue, setCurrencyInputValue] = useState(0);
+
+  // const handleCurrencyInputChange: any = (value: string) => {
+  //   const currencyRegex = new RegExp(
+  //     '^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:.[0-9]{1,2})?$',
+  //   );
+
+  //   const isCurrencyCompliant = currencyRegex.test(value);
+
+  //   console.log(isCurrencyCompliant);
+
+  //   if (!value) {
+  //     setCurrencyInputValue(0);
+  //     setCurrencyInputLabel('');
+  //     setCurrencyInputError('');
+  //     return;
+  //   }
+
+  //   if (!isCurrencyCompliant) {
+  //     console.log(1.1)
+  //     setCurrencyInputValue(0);
+  //     setCurrencyInputLabel('fucked');
+  //     setCurrencyInputError('Invalid format');
+  //   }
+
+  //   const parsedValue = value.replace(',', '');
+  //   const num = Number(parsedValue);
+  //   setCurrencyInputValue(num);
+  //   setCurrencyInputLabel(value);
+  //   setCurrencyInputError('');
+  // };
 
   const handleRenderFilters = (filters: FilterItem[]) => {
     return filters.map(({ id, name, label, type, placeholder }: FilterItem) => {
@@ -32,20 +103,47 @@ const Filters = ({ items = [] }: IBFiltersProps) => {
           );
 
         case 1:
-          return <div key={id}>1</div>;
+          return (
+            <TextField
+              styles={textBoxStyles}
+              key={id}
+              name={name}
+              placeholder={placeholder ?? 'Number'}
+              onChange={(e: any) => handleNumericInputChange(e.target.value)} // TODO remove any
+              onGetErrorMessage={() => numberInputError}
+            />
+          );
 
         case 2:
           return <div key={id}>2</div>;
 
         case 3:
-          return <div key={id}>3</div>;
+          return (
+            // <TextField
+            //   styles={textBoxStyles}
+            //   key={id}
+            //   name={name}
+            //   placeholder={placeholder ?? 'Currency'}
+            //   onChange={(e: any) => handleCurrencyInputChange(e.target.value)} // TODO remove any
+            //   onGetErrorMessage={() => currencyInputError}
+            //   value={currencyInputLabel}
+            // />
+            <></>
+          );
 
         default:
           return <div key={id}>0</div>;
       }
     });
   };
-  return <IBBox mb={4}>{handleRenderFilters(items)}</IBBox>;
+
+  return (
+    <IBBox mb={4}>
+      <h1>number: {numberInputState}</h1>
+      {/* <h1>currency: {currencyInputValue}</h1> */}
+      {handleRenderFilters(items)}
+    </IBBox>
+  );
 };
 
 export default Filters;
