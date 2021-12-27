f8293f8995c2768ae449651b400ee442d5c66cb5
palloncino <antonio.guiotto.dev@gmail.com>
2021-12-27 12:13:40 +0100

    filters pt. 2

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,32 +1,29 @@
 /* eslint-disable */
-import React from "react";
+import React from 'react';
 import { IBFiltersProps, FilterItem } from './filters.types';
 
-const Filters = ({
-  items = [],
-}: IBFiltersProps) => {
-  return (
-    <div id="filters-wrapper">
-      {items.map(({ id, name, label, type }: FilterItem) => {
-        switch (type) {
-          case 0:
-            return <div>0</div>
+const Filters = ({ items = [] }: IBFiltersProps) => {
+  const handleRenderFilters = (filters: FilterItem[]) => {
+    return filters.map(({ id, name, label, type }: FilterItem) => {
+      switch (type) {
+        case 0:
+          return <div key={id}>0</div>;
 
-          case 1:
-            return <div>1</div>
+        case 1:
+          return <div key={id}>1</div>;
 
-          case 2:
-            return <div>2</div>
+        case 2:
+          return <div key={id}>2</div>;
 
-          case 3:
-            return <div>3</div>
-        
-          default:
-            return <div>0</div>
-        }
-      })}
-    </div>
-  );
+        case 3:
+          return <div key={id}>3</div>;
+
+        default:
+          return <div key={id}>0</div>;
+      }
+    });
+  };
+  return <div id="filters-wrapper">{handleRenderFilters(items)}</div>;
 };
 
 export default Filters;
