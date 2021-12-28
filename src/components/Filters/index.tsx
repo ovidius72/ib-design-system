00792aa5f653a108400bb527fe98658a407e2cfc
palloncino <antonio.guiotto.dev@gmail.com>
2021-12-28 12:56:49 +0100

    filters pt. 8

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -29,26 +29,41 @@ const Filters = ({ setFilterState, items = [] }: IBFiltersProps) => {
   };
   
   const preHandleSetStateCurrency = ( fieldName: string, value: string | undefined ) => {
-    
+
+    // cleanup when empty
     if (!value) {
+      console.log('empty!')
       return setState({ ...state, [fieldName]: { value: '', fieldError: '' } })
     };
 
+    // commas from client not allowed
+    while (value?.includes(',')) {
+      value = value.replace(',', '');
+    }
+
+    // error if not a valid number
     if (Number.isNaN(Number(value))) {
+      console.log('invalid!')
       return setState({ ...state, [fieldName]: { value: '', fieldError: 'Invalid input' } })
     };
 
+    // remove zeros upfront
     while (value[0] === '0') {
       value = value.substring(1);
     }
 
+    // if decimal, controls for two digits max.
     const decimals = value.split('.')[1];
-
     if (decimals && decimals.length > 2) return;
 
+    // set parsed string
     return setState({ ...state, [fieldName]: { value: value, fieldError: '' } });
 
   };
+
+  const renderInputCurrency = (str: string) => {
+    return str.replace(/^\d+(?=.|$)/, (int) => int.replace(/(?=(?:\d{3})+$)(?!^)/g, ","));
+  }
 
   const handleRenderFilters = (filters: FilterItem[]) => {
     return filters.map(({ id, name, label, type, placeholder, prefix }: FilterItem) => {
@@ -90,7 +105,7 @@ const Filters = ({ setFilterState, items = [] }: IBFiltersProps) => {
                 preHandleSetStateCurrency(name, e.target.value)
               }
               onGetErrorMessage={() => state[name]?.fieldError}
-              value={state[name]?.value ?? ''}
+              value={renderInputCurrency(state[name]?.value ?? '')}
             />
           );
 
